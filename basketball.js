4335a897c484cfbf3d0633a2b9a63d1cccf5c14d
sujeethakalimuthu <50768606+sujeethakalimuthu@users.noreply.github.com>
2023-02-04 12:07:37 +1000

    Add files via upload

---
+++ basketball.js
@@ -0,0 +1,29 @@
+status = "";
+function preload()
+{
+    img = loadImage("basketball.png");
+}
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Detecting Objects";
+}
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    objectDetector.detect(img, gotResults);
+    status = true;
+}
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+    }
+}
